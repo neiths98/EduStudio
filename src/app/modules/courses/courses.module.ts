aac525c5628eb137ad551ed855943b528724e08e
nathan <nathan_santos@usp.br>
2021-12-29 23:25:45 -0300

    adicionando componente course-card

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 
 @NgModule({
   declarations: [
-    CoursesListComponent
+    CoursesListComponent,
+    CourseCardComponent
   ],
   imports: [
     CommonModule,
