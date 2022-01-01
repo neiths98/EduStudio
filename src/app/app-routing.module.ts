eeb92835a5bdf098f4d2c1f52d61cd2868ddf01d
nathan <nathan_santos@usp.br>
2022-01-01 14:27:04 -0300

    adicionando rotas para a pagina de novo curso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
   { path: 'courses', loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule) },
+  { path: 'course', loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule) },
 ];
 
 @NgModule({
