05764396ca51380434fdfa6c71a8c158e6532650
nathan <nathan_santos@usp.br>
2022-01-06 20:40:00 -0300

    adicionando sidebar de aulas ao course-view

--- src/app/modules/course/course-view/course-view.component.ts
+++ src/app/modules/course/course-view/course-view.component.ts
@@ -6,9 +6,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseViewComponent implements OnInit {
 
+  course = {
+    id: '124',
+    name: 'Curso de Motoboy',
+    author: 'Nero',
+    thumbnail: 'https://pbs.twimg.com/media/DdaNfBJWkAI-qlq.jpg',
+    classes: [
+      { title: 'Aula 1 - Yodinha', link: '../../../../assets/yoda.MP4', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolores officiis laboriosam qui similique doloribus, accusantium quasi enim. Sunt laborum repellat, voluptates facere nihil odit hic eligendi.' },
+      { title: 'Aula 2 - Yodinha', link: '../../../../assets/tiktok.MP4' },
+      { title: 'Aula 3 - Yodinha', link: '../../../../assets/yoda.MP4' },
+      { title: 'Aula 4 - Yodinha', link: '../../../../assets/tiktok.MP4' },
+      { title: 'Aula 5 - Yodinha', link: '../../../../assets/yoda.MP4' },
+    ],
+    url: 'courses/124',
+    length: 8
+  };
+
+  currentClass = this.course.classes[0];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateCurrentClassOnClassClick(currentClass: any) {
+    this.currentClass = currentClass;
   }
 
 }
