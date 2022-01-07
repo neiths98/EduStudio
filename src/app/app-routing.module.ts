d0bb5f7330da75ce10f4752b2df1b0a2df4cc5ab
nathan <nathan_santos@usp.br>
2022-01-07 19:30:45 -0300

    adicionando app-root
    reorganizando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRootComponent } from './app-root/app-root.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'courses', pathMatch: 'full' },
-  { path: 'courses', loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule) },
-  { path: 'course', loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule) },
-  { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
+  { 
+    path: '', component: AppRootComponent,
+    children: [
+      { path: '', loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule) },
+      { path: 'courses', loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule) },
+      { path: 'course', loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule) },
+      { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
+    ]
+  },
 ];
 
 @NgModule({
