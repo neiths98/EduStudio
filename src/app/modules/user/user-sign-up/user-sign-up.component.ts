40d8f738f1a7185aafe9d44efd01e064c7436187
nathan <nathan_santos@usp.br>
2022-02-17 15:41:59 -0300

    adicao de validacoes no formulario de cadastro

--- src/app/modules/user/user-sign-up/user-sign-up.component.ts
+++ src/app/modules/user/user-sign-up/user-sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AppValidators } from 'src/app/shared/widgets/app-forms/validators/app.validators';
 
 @Component({
   selector: 'app-user-sign-up',
@@ -6,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSignUpComponent implements OnInit {
 
-  constructor() { }
+  readonly lengthMaxEmail: number = AppValidators.constants.LENGTH_MAX_EMAIL;
+  readonly lengthMinPassword: number = AppValidators.constants.LENGTH_MIN_PASSWORD;
+  readonly lengthMaxPassword: number = AppValidators.constants.LENGTH_MAX_PASSWORD;
+
+  signUpFormSubmitted: boolean = false;
+  signUpForm!: FormGroup;
+  emailField!: FormControl;
+  passwordField!:FormControl;
+  confirmPasswordField!: FormControl; 
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signUpFormInit();
+  }
+
+  signUpFormInit() {
+    this.emailField = new FormControl('', [
+      Validators.required,
+      AppValidators.emailPattern(),
+      Validators.maxLength(this.lengthMaxEmail)
+    ]);
+
+    this.passwordField = new FormControl('', [
+      Validators.required,
+      Validators.minLength(this.lengthMinPassword),
+      Validators.maxLength(this.lengthMaxPassword)
+    ]);
+
+    this.confirmPasswordField = new FormControl('', [
+      Validators.required,
+      AppValidators.isNotEqualTo(this.passwordField)
+    ]);
+
+    this.signUpForm = this.fb.group({
+      emailField: this.emailField,
+      passwordField: this.passwordField,
+      confirmPasswordField: this.confirmPasswordField,
+    });
+  }
+
+  signUpFormSubmit() {
+    this.signUpFormSubmitted = true;
   }
 
 }
