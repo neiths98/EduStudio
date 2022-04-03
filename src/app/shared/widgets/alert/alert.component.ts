f9e2f3b75928988896283c0d76598082541c976d
nathan <nathan_santos@usp.br>
2022-04-03 02:03:09 -0300

    ADD: criacao do componente alert

---
+++ src/app/shared/widgets/alert/alert.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html'
+})
+export class AlertComponent implements OnInit {
+
+  readonly typeClasses: any = {
+    error: {
+      class: 'alert-danger',
+      icon: '../../../../assets/icons/exclamation-triangle-fill.svg'
+    },
+    info: {
+      class: 'alert-info',
+      icon: '../../../../assets/icons/info-circle-fill.svg'
+    },
+    success: {
+      class: 'alert-success',
+      icon: '../../../../assets/icons/check-circle-fill.svg'
+    },
+    warning: {
+      class: 'alert-warning',
+      icon: '../../../../assets/icons/exclamation-triangle-fill.svg'
+    }
+  };
+
+  readonly defaultMessage = 'Houve um erro inesperado, tente novamente'
+
+  @Input() type = 'info';
+
+  @Input() isDismissible = true;
+
+  @Input() message: string = this.defaultMessage;
+
+  @Input() isClosed: boolean = true;
+
+  classes: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void { 
+    const typeClass = this.typeClasses[this.type].class;
+    this.classes.push(typeClass);
+  }
+
+  showAlert() {
+    this.isClosed = false;
+  }
+
+  onClose() {
+    this.isClosed = true;
+  }
+
+}
--- src/app/shared/widgets/widgets.module.ts
+++ src/app/shared/widgets/widgets.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonLoadingComponent } from './button-loading/button-loading.component';
+import { AlertComponent } from './alert/alert.component';
 
 
 
 @NgModule({
   declarations: [
-    ButtonLoadingComponent
+    ButtonLoadingComponent,
+    AlertComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ButtonLoadingComponent
+    ButtonLoadingComponent,
+    AlertComponent
   ]
 })
 export class WidgetsModule { }
