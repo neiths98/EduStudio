b0173658bbc77e2b132c6886a857a1a5b6ef0c6b
nathan <nathan_santos@usp.br>
2022-04-03 20:58:27 -0300

    ADD: adicionando guard para usuarios logados

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedInAuthGuard } from 'src/app/core/guards/logged-in-auth.guard';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 
 const routes: Routes = [
-  { path: 'sign-up', component: UserSignUpComponent },
-  { path: 'login', component: UserLoginComponent },
+  { path: 'sign-up', component: UserSignUpComponent, canActivate: [LoggedInAuthGuard] },
+  { path: 'login', component: UserLoginComponent, canActivate: [LoggedInAuthGuard] },
 ];
 
 @NgModule({
